d09ea5fc0f81732be1d731f1ce1b3fd40d3abdbb
ernie <edance1775@gmail.com>
2022-01-22 12:49:49 -0500

    question 13 is solved

--- AvengerPracticeHour/Day_06_01_22_2022/question13/APITest.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question13/APITest.java
@@ -3,5 +3,9 @@ package Day_06_01_22_2022.question13;
 public class APITest extends FunctionalTest{
 
     // implements code here
+     protected void print(){
+        // super.print();
+         System.out.println("I am doing API testing");
+     }
 
 }
