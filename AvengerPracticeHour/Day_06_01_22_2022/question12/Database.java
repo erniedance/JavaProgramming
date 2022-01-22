cd20d5363dea3811225390d92faeda223e493e02
ernie <edance1775@gmail.com>
2022-01-22 12:42:30 -0500

    question 12 is solved

--- AvengerPracticeHour/Day_06_01_22_2022/question12/Database.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question12/Database.java
@@ -1,11 +1,11 @@
 package Day_06_01_22_2022.question12;
 
 public class Database {
-    private String name; // line 1
+   private String name; // line 1
 
-    public static String getName(){
-//        return name; // line 2
-        return null;
+    public  String getName(){
+       return name; // line 2
+
     }
 
     public void setName (String name) {
